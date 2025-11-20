6c4c7d9e861c06e0424f3b10c5dfc2edaca73b55
ThaiNV32 <thainv32@fpt.com>
2025-11-20 14:12:28 +0700

    init commit

---
+++ src/main/java/com/thai27/split_bill_be/SplitBillBeApplication.java
@@ -0,0 +1,13 @@
+package com.thai27.split_bill_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitBillBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitBillBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thai27/split_bill_be/SplitBillBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thai27.split_bill_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitBillBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
